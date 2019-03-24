e63c4c548771153cdcdfdc092af90a88b3a5329c
Andrew <andro00460@gmail.com>
2019-03-24 14:27:13 +0600

    conf

--- db/migrate/20190320131129_add_comments_permit.rb
+++ db/migrate/20190320131129_add_comments_permit.rb
@@ -2,9 +2,7 @@ class AddCommentsPermit < ActiveRecord::Migration[5.2]
   
     def change
       change_column_default :posts, :comments_permit, from: true, to: false
-      remove_column :posts, :comment_permit, :boolean, default: false
-      
-     
+          
     end
   
 end
--- db/schema.rb
+++ db/schema.rb
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 2019_03_22_075149) do
     t.datetime "updated_at", null: false
     t.integer "user_id"
     t.boolean "comments_permit", default: false
+    t.boolean "comment_permit", default: false
     t.integer "cached_votes_total", default: 0
     t.integer "cached_votes_score", default: 0
     t.integer "cached_votes_up", default: 0
